8f2909022f58c575e1f8fb114acec1703fe841b0
Windows 10 <https://github.com/youseftarekk?tab=repositories>
2019-09-23 18:53:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gocar/AppConfig.java
@@ -0,0 +1,8 @@
+package com.example.gocar;
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://172.20.10.4/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://172.20.10.4/android_login_api/register.php";
+}
